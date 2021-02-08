d5705f8500688b06029281627987cb9d8736b1f2
Sidhant <sidhantm.ic.17@nsit.net.in>
2021-02-08 10:35:04 +0530

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Sidhant Madan on 08/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
